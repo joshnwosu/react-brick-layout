b420833ab6cd993c8afc34d2bd4e43f82c21cb78
joshnwosu <joshuanwosu078@gmail.com>
2023-01-23 04:04:43 +0100

    update: dependencies

--- test/common.test.tsx
+++ test/common.test.tsx
@@ -9,7 +9,7 @@ describe('Common render', () => {
   it('renders without crashing', () => {
     render(
       <BrickLayout>
-        <div>Children</div>
+        <div>Hello</div>
       </BrickLayout>,
     )
   })
