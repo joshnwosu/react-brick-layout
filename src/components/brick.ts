046ecd1f9689c8a768ca7e3fc45c3831858dde3d
joshnwosu <joshuanwosu078@gmail.com>
2023-01-24 16:22:08 +0100

    feat: response | validate | extend

--- src/components/brick.ts
+++ src/components/brick.ts
@@ -4,7 +4,7 @@ type Props = {
   gutter?: number
   column?: number
   breakPoint?: {
-    [key: number]: number
+    [key: string]: any
   }
   rtl?: boolean
   className?: string
@@ -23,6 +23,17 @@ const defaultProps = {
 }
 
 export class Brick implements Props {
+  container: HTMLDivElement | null
+  itemNodeList: HTMLCollection | undefined
+  gutter: number
+  breakPoint?: { [key: string]: any } | undefined
+  rtl?: boolean | undefined
+  column?: number | undefined
+  style?: React.CSSProperties | undefined
+  animateOnResize?: boolean | undefined
+  props: {}
+  d_props: {}
+
   constructor({
     gutter = defaultProps.gutter,
     column = defaultProps.column,
@@ -32,6 +43,53 @@ export class Brick implements Props {
     animateOnResize = defaultProps.animateOnResize,
     style = defaultProps.style,
   }) {
-    console.log({ gutter, column, breakPoint, rtl, className, animateOnResize, style })
+    this.container = document.querySelector(className)
+    this.itemNodeList = this.container?.children
+    this.gutter = this.validate(gutter, 10)
+    this.column = this.validate(column, 3)
+    this.breakPoint = breakPoint
+
+    this.d_props = {
+      gutter,
+      column,
+      breakPoint,
+      rtl,
+      className,
+      animateOnResize,
+      style,
+    }
+
+    this.props = this.extend(this.d_props, {
+      container: this.container,
+      items: this.itemNodeList,
+      gutter: this.gutter,
+      column: this.column,
+    })
+
+    console.log('Props: ', this.props)
+  }
+
+  extend(a: any, b: any) {
+    for (var key in b) {
+      if (b.hasOwnProperty(key)) {
+        a[key] = b[key]
+      }
+    }
+    return a
+  }
+
+  validate(value: number, defaultValue: number) {
+    return typeof value === 'number' && isFinite(value) && Math.floor(value) === value ? value : defaultValue
+  }
+
+  responsive() {
+    // const width = window.innerWidth;
+    for (const key in this.breakPoint) {
+      if (this.breakPoint.hasOwnProperty(key)) {
+        if (window.innerWidth >= Number(key)) {
+          this.column = this.breakPoint[key]
+        }
+      }
+    }
   }
 }
